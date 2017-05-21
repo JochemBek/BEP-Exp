b64f086cccb13ccbba284329fe14464acf72da1b
JochemBek <Jochem_Bek@hotmail.com>
2017-05-21 15:27:01 +0200

    MeDoo for selectMeasures

--- suserecommender/ajax/selectMeasures.php
+++ suserecommender/ajax/selectMeasures.php
@@ -1,23 +1,11 @@
 <?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "vlabnl_eb";
 
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check connection
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	} 
+require 'medoo.min.php';
 
-	$sth = mysqli_query($conn, "SELECT * FROM measures");
-	$rows = array();
-	while($r = mysqli_fetch_assoc($sth)) {
-	    $rows[] = $r;
-	}
-	print json_encode($rows);
-	
-	
-	$conn->close();
+$database = new medoo();
+
+$data = $database->select("measures","*");
+
+echo json_encode($data);
+
 ?>
