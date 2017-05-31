c892aaf7a8a7bc6486e82abf6356133f09748f28
Janissen <b.n.j.janissen@student.tue.nl>
2017-05-31 23:27:46 +0200

    Disabled click trough in extraQuestionView
    Thou shall first answer!

--- suserecommender/js/controller/extraQuestionsController.js
+++ suserecommender/js/controller/extraQuestionsController.js
@@ -1,18 +1,21 @@
 var ExtraQuestionsController = function(model, view) {
-	
+
 	view.volgendeButton.click(function(){
+		if( $( '#extraQuestions .extraQuestions-group input:checked' ).length < 3 ){
+			alert( 'U bent waarschijnlijk een vraag vergeten!' );
+		}	else {
 		$('#extraQuestions .list-group-item').each(function() {
 			var qs = $(this).attr('id');
 			var order = $("#extraQuestions .list-group-item").index(this)+1;
-			
+
 			var val = $(this).find('input:checked').val();
-	
+
 			console.log("Value of measure " + qs + " is " + val);
-			
+
 			model.setExtraQuestion(qs, order, val);
 		});
 		model.extraQuestionsDone();
-		
-	});
-	
+
+	}
+});
  }
