2b996f2bc0691bd399eb823df8863f212f124790
JochemBek <Jochem_Bek@hotmail.com>
2017-06-01 02:35:27 +0200

    ExtraQuestionsView now scalable

--- suserecommender/js/view/extraQuestionsView.js
+++ suserecommender/js/view/extraQuestionsView.js
@@ -21,32 +21,32 @@ var ExtraQuestionsView = function (model, container){
 	displayRecomQuestions = function(){
 		var recommendations = model.getRecommendations();
 
-		var legend 					= $( "<div id='legends' class='row'><div class='col-md-6'></div><div class='col-md-2'><p>1. Ik zou graag meer informatie willen ontvangen over deze maatregel.</p></div> <div class='col-md-2'><p>2. Ik voer deze maatregel al uit.</p></div><div class='col-md-2'><p>3. Geen van beide.</p></div>" );
+		var legend 					= $( "<div id='legends' class='row'><div class='col-xs-4 col-sm-4 col-md-4'></div><div class='col-xs-3 col-sm-3 col-md-3'><p>1. Ik zou graag meer informatie willen ontvangen over deze maatregel.</p></div> <div class='col-xs-3 col-sm-3 col-md-3'><p>2. Ik voer deze maatregel al uit.</p></div><div class='col-xs-2 col-sm-2 col-md-2'><p>3. Geen van beide.</p></div>" );
 		extraQuestionList.append(legend);
 
 		$.each(recommendations, function(key, value) {
 			var item = $("<div class='list-group-item row'>");
 				item.attr('id', value.id);
-			var textContainer = $("<div class='col-md-6'>");
+			var textContainer = $("<div class='col-xs-4 col-sm-4 col-md-4'>");
 			var text = $( "<p class='list-group-item-text'>" );
 				text.html(value.infinitive);
 				textContainer.append(text)
 			item.append(textContainer);
 
 
-			var emailCheckboxContainer =  $( "<div class='col-md-2'>" );
+			var emailCheckboxContainer =  $( "<div class='col-xs-3 col-sm-3 col-md-3'>" );
 				var checkboxEmail = $( "<input type='radio' name='" + value.id + "' value='1' class='wantEmail'>" );
 			emailCheckboxContainer.append( checkboxEmail );
 
 			item.append( emailCheckboxContainer );
 
-			var alreadyDoCheckboxContainer =  $( "<div class='col-md-2'>" );
+			var alreadyDoCheckboxContainer =  $( "<div class='col-xs-3 col-sm-3 col-md-3'>" );
 				var checkboxAlreadyDo = $( "<input type='radio' name='" + value.id + "' value='2' class='alreadyDo'>" );
 			alreadyDoCheckboxContainer.append( checkboxAlreadyDo );
 
 			item.append( alreadyDoCheckboxContainer );
 			
-			var neitherCheckboxContainer =  $( "<div class='col-md-2'>" );
+			var neitherCheckboxContainer =  $( "<div class='col-xs-2 col-sm-2 col-md-2'>" );
 				var checkboxNeither = $( "<input type='radio' name='" + value.id + "' value='3' class='neither'>" );
 			neitherCheckboxContainer.append( checkboxNeither );
 
